eb4d41eb95ab01577fce81b35fc1047c75713e47
vildeil <vildeludvigsen@hotmail.com>
2024-01-16 15:50:36 +0100

    jobber videre
    Lagt til artikkelkort og koblet sammen med js

--- shopscript.js
+++ shopscript.js
@@ -1,2 +1,16 @@
-document.getElementById("test").innerHTML = "Det funker!"
+console.log(products)
 
+let productsHTML = "<h2>Ninjago</h2>"
+
+products.map(product => productsHTML += `<article>
+<img src="website_images/PROD_${product.imagefile}" alt="${product.title}">
+<a href="#">${product.category}</a>
+<h3>${product.title}</h3>
+<span>Kr. ${product.price}</span>
+<button>Legg i handlekurv</button>
+</article>`)
+
+console.log(productsHTML)
+
+const main = document.getElementsByTagName("main")
+main[0].innerHTML = productsHTML
